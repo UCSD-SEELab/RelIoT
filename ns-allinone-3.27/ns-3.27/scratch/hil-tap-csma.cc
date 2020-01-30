3c0e4ed4d211693f36c479e07fe0c460c3aabdbd
Orienfish <x1yu@ucsd.edu>
2020-01-29 20:20:33 -0800

    communicate on both direction with pi

--- ns-allinone-3.27/ns-3.27/scratch/hil-tap-csma.cc
+++ ns-allinone-3.27/ns-3.27/scratch/hil-tap-csma.cc
@@ -64,16 +64,77 @@
  #include "ns3/internet-module.h"
  #include "ns3/ipv4-global-routing-helper.h"
  #include "ns3/tap-bridge-module.h"
+ #include "ns3/applications-module.h"
+ #include "ns3/internet-apps-module.h"
 
  using namespace ns3;
 
- NS_LOG_COMPONENT_DEFINE ("TapCsmaExample");
+#define packetSize 20
+
+ NS_LOG_COMPONENT_DEFINE ("HILTapCsmaExample");
+
+ static inline std::string
+ PrintReceivedPacket (Address& from, Ptr<Packet> packet)
+ {
+   InetSocketAddress iaddr = InetSocketAddress::ConvertFrom (from);
+   uint8_t buf[packetSize];
+   packet->CopyData(buf, packetSize);
+
+   std::ostringstream oss;
+   oss << "--\nReceived one packet! From socket: " << iaddr.GetIpv4 ()
+       << " port: " << iaddr.GetPort ()
+       << " at time = " << Simulator::Now ().GetSeconds ()
+       << "\n" << buf
+       << "\n--";
+
+   return oss.str ();
+ }
+
+ /**
+  * \param socket Pointer to socket.
+  *
+  * Packet receiving sink.
+  */
+ void
+ ReceivePacket (Ptr<Socket> socket)
+ {
+   Ptr<Packet> packet;
+   Address from;
+   while ((packet = socket->RecvFrom (from)))
+     {
+       if (packet->GetSize () > 0)
+         {
+           NS_LOG_UNCOND (PrintReceivedPacket (from, packet));
+         }
+     }
+ }
+
+ /**
+  * \param socket Pointer to socket.
+  * \param pktSize Packet size.
+  * \param n Pointer to node.
+  * \param pktCount Number of packets to generate.
+  * \param pktInterval Packet sending interval.
+  *
+  * Traffic generator.
+  */
+ static void
+ GenerateTraffic (Ptr<Socket> socket, Time pktInterval)
+ {
+   Ptr<Packet> pkt = Create<Packet> (reinterpret_cast<const uint8_t*> ("hello"), 5);
+   socket->Send (pkt);
+   Simulator::Schedule (pktInterval, &GenerateTraffic, socket, pktInterval);
+ }
 
  int
  main (int argc, char *argv[])
  {
    std::string mode = "UseBridge";
    std::string tapName = "tap0";
+   double interval = 1;          // seconds
+   double startTime = 0.0;       // seconds
+   // Convert to time object
+   Time interPacketInterval = Seconds (interval);
 
    CommandLine cmd;
    cmd.AddValue ("mode", "Mode setting of TapBridge", mode);
@@ -83,31 +144,57 @@
    GlobalValue::Bind ("SimulatorImplementationType", StringValue ("ns3::RealtimeSimulatorImpl"));
    GlobalValue::Bind ("ChecksumEnabled", BooleanValue (true));
 
+   // explicitly create four nodes
    NodeContainer nodes;
    nodes.Create (4);
 
+   // create csma channel on physical layer
    CsmaHelper csma;
    csma.SetChannelAttribute ("DataRate", DataRateValue (5000000));
    csma.SetChannelAttribute ("Delay", TimeValue (MilliSeconds (2)));
 
    NetDeviceContainer devices = csma.Install (nodes);
 
+   // add an ip stack to all nodes
    InternetStackHelper stack;
    stack.Install (nodes);
 
-   Ipv4AddressHelper addresses;
-   addresses.SetBase ("10.1.1.0", "255.255.255.0");
-   Ipv4InterfaceContainer interfaces = addresses.Assign (devices);
+   // assign ip addresses
+   Ipv4AddressHelper ipv4;
+   ipv4.SetBase ("10.1.1.0", "255.255.255.0");
+   Ipv4InterfaceContainer interfaces = ipv4.Assign (devices);
 
+   // add a UDP receiver to node 3 (10.1.1.4)
+   uint8_t revNode = 3;
+   uint16_t revPort = 4000;
+   TypeId tid = TypeId::LookupByName ("ns3::UdpSocketFactory");
+   Ptr<Socket> recvSink = Socket::CreateSocket (nodes.Get (revNode), tid);
+   InetSocketAddress local = InetSocketAddress (Ipv4Address::GetAny (), revPort);
+   recvSink->Bind (local);
+   recvSink->SetRecvCallback (MakeCallback (&ReceivePacket));
+
+   // add a UDP sender on node 2 (10.1.1.3) toward piNode (10.1.1.5)
+   uint8_t sendNode = 2;
+   char piAddr[] = "10.1.1.5";
+   uint16_t piPort = 4000;
+   Ptr<Socket> source = Socket::CreateSocket (nodes.Get (sendNode), tid);
+   // InetSocketAddress remote = InetSocketAddress (Ipv4Address::GetBroadcast (), piPort);
+   InetSocketAddress remote = InetSocketAddress (Ipv4Address (piAddr), piPort);
+   source->SetAllowBroadcast (true);
+   source->Connect (remote);
+
+   // enable routing tables
+   csma.EnablePcapAll ("tap-csma", false);
+   Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+   // set tap bridge to connect to external pi
    TapBridgeHelper tapBridge;
    tapBridge.SetAttribute ("Mode", StringValue (mode));
    tapBridge.SetAttribute ("DeviceName", StringValue (tapName));
    tapBridge.Install (nodes.Get (0), devices.Get (0));
 
-   csma.EnablePcapAll ("tap-csma", false);
-   Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
-
-   Simulator::Stop (Seconds (60.));
+   Simulator::Schedule (Seconds (startTime), &GenerateTraffic, source, interPacketInterval);
+   Simulator::Stop (Seconds (30.));
    Simulator::Run ();
    Simulator::Destroy ();
  }
